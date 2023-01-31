d4d96327c2a4f29742d4ac10ecbd512fdaef49c2
hasanuyanik <hasanuyanik@outlook.com>
2023-01-31 22:59:32 +0300

    [chore] Create spring project

---
+++ self-service-app/src/test/java/com/hsn/selfserviceapp/SelfServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsn.selfserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
